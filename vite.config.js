0a09cc568422dca8dda2285e40dd501c16279612
Kazan <andyswebking@gmail.com>
2022-09-10 17:57:26 +0800

    Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
+import typescript2 from 'rollup-plugin-typescript2'
+import image from '@rollup/plugin-image'
 import vue from '@vitejs/plugin-vue'
+const { resolve } = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: './',
+  plugins: [
+    vue(),
+    { ...image(), enforce: 'pre' },
+    { ...typescript2(), apply: 'build' }
+  ],
+  resolve: {
+    alias: {
+      '/images': 'src/assets/images',
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/base.scss";`
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        article: resolve(__dirname, 'article/index.html')
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
 })
